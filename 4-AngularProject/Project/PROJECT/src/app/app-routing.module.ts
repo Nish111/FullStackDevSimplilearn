aed43e76075799f24eca1cbe9e87b87690b647e6
Nishannt <banginwar.nishant1@gmail.com>
2022-09-12 10:01:16 +0530

    part4

---
+++ 4-AngularProject/Project/PROJECT/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { LoginComponent } from './routes/login/login.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { PasswordComponent } from './routes/password/password.component';
+import { ChartsComponent } from './routes/charts/charts.component';
+
+const routes: Routes = [
+  {path: '', component:NavbarComponent},
+  {path: 'static', component:ToggleComponent, pathMatch: 'full'},
+  {path: 'login', component:LoginComponent},
+  {path: 'register', component:RegisterComponent},
+  {path: 'changepassword', component:PasswordComponent},
+  {path: 'charts', component:ChartsComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ 4-AngularProject/Project/PROJECT/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToggleComponent } from './toggle/toggle.component';
+import { RoutesComponent } from './routes/routes.component';
+import { ChartsComponent } from './routes/charts/charts.component';
+import { LoginComponent } from './routes/login/login.component';
+import { RegisterComponent } from './routes/register/register.component';
+import { PasswordComponent } from './routes/password/password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ToggleComponent,
+    RoutesComponent,
+    ChartsComponent,
+    LoginComponent,
+    RegisterComponent,
+    PasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
